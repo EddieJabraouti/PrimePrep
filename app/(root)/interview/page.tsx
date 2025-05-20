6d78f1f8d625ece63a420628cea8203bfc0427d7
Eddiejab <90640761+Eddiejab@users.noreply.github.com>
2025-05-19 22:25:18 -0400

    very start of the Interview UI and Agent implementation.

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agents from "@/components/Agents";
+import React from 'react'
+
+const Page = () => {
+    return (
+        <>
+            <h3>Generate Interview</h3>
+            <Agents userName="You" userId="user1" type="generate"/>
+        </>
+
+    )
+}
+export default Page
